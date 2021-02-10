821476be92e91074ee6ed04d1b07f294edeedffd
gabrielsouzahub <gabriel_souza@unigranrio.br>
2021-02-10 12:49:55 -0300

    Busca por filmes pela barra, img e criação de pagina do filme

--- js/views/FilmView.js
+++ js/views/FilmView.js
@@ -5,7 +5,27 @@ class FilmView{
     update(model){
         this.container.innerHTML = 
         `<p>${model.title}</p>
-         <img src=${model.poster}>  
+         <img src=${model.poster} onclick="FilmController.buscaFilmeImg('${model.title}')">  
         `
+    }
+    updatePesquisa(model){
+        this.container.innerHTML =
+        `
+        <p>${model.title}</p>
+        <img src=${model.poster}>
+        <p>${model.released}</p>
+        <p>${model.runtime}</p>
+        <p>${model.genre}</p>
+        <p>${model.director}</p>
+        <p>${model.writter}</p>
+        <p>${model.actors}</p>
+        <p>${model.plot}</p>
+        <p>${model.language}</p>
+        <p>${model.country}</p>
+        <p>${model.awards}</p>
+        <p>${model.boxOffice}</p>
+        <p>${model.production}</p>
+        `
+        console.log(model.ratings);
     }
 }
